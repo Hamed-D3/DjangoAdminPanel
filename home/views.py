395c59cf8a27c0b91e75dee6dfec3f8024c0c16c
Hamed_D3 <hameddaneshvar3@gmail.com>
2022-04-16 21:12:46 +0430

    separate sidebar and fix setting links in sidebar

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView, UpdateView
 from home.models import WebSiteSetting, AboutUsSetting, ContactUsSetting
 
+from profiles.views import get_setting_sidebar
+
 from home.forms import WebSiteSettingForm, AboutUsSettingForm, ContactUsSettingForm
 
 # Create your views here.
@@ -11,7 +13,9 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         if WebSiteSetting.objects.all():
             context['setting'] = WebSiteSetting.objects.all()[0]
+            context['settings'] = get_setting_sidebar()
         else:
+            context['settings'] = get_setting_sidebar()
             context['setting'] = {
                 'name':'setting',
                 'url':'http://site.com',
@@ -28,6 +32,7 @@ class WebSiteSettingView(UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['settings'] = get_setting_sidebar()
         context['verbose_name'] = WebSiteSetting._meta.verbose_name_raw
         return context
 
@@ -39,6 +44,7 @@ class AboutUsSettingView(UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['settings'] = get_setting_sidebar()
         context['verbose_name'] = AboutUsSetting._meta.verbose_name_raw
         return context
 
@@ -51,5 +57,6 @@ class ContactUsSettingView(UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['settings'] = get_setting_sidebar()
         context['verbose_name'] = ContactUsSetting._meta.verbose_name_raw
         return context
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,30 @@
 from django.views.generic import TemplateView
 
+# models
+from home.models import WebSiteSetting, ContactUsSetting, AboutUsSetting
+
 # Create your views here.
+def get_setting_sidebar():
+    setting_info = {}
+
+    setting = WebSiteSetting.objects.all()[0]
+    setting_info['general'] = {'name': WebSiteSetting._meta.verbose_name_raw, 'slug': setting.slug, 'type': 'general'}
+
+    setting = ContactUsSetting.objects.all()[0]
+    setting_info['contactus'] = {'name': ContactUsSetting._meta.verbose_name_raw, 'slug': setting.slug, 'type': 'contactus'}
+
+    setting = AboutUsSetting.objects.all()[0]
+    setting_info['aboutus'] = {'name': AboutUsSetting._meta.verbose_name_raw, 'slug': setting.slug, 'type': 'aboutus'}
+
+    return setting_info
+
+
+
 class IndexView(TemplateView):
     template_name = 'profiles/index.html'
+
+    # get name of setting, slug and it type and send it to the template    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['settings'] = get_setting_sidebar()
+        return context
